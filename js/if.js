c6704497978230b7e46b9ad4e5280b9ee148f03a
JordanSanchez <95088080+Jordanjisv@users.noreply.github.com>
2023-10-18 03:05:26 -0500

    if.js

--- js/if.js
+++ js/if.js
@@ -30,13 +30,13 @@ tijeraBtn.addEventListener("click", () => {
 
 function juego(opcionUsuario) {
 
-    usuarioImg.src = "../img/if/" + opcionUsuario + ".png";
+    usuarioImg.src = "../img/if/" +opcionUsuario+ ".png";
 
     resultado.innerHTML = "JUGANDO";
 
     const intervalo = setInterval(function(){
         const opcionMaquina = calcOpcionMaquina();
-        maquinaImg.src = "../img/if/" + opcionMaquina + ".png";
+        maquinaImg.src = "../img/if/" +opcionMaquina+ ".png";
     }, 100);
 
     setTimeout(function () {
